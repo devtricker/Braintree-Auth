da39f51027025c8e831fcf5faf76dfced8c53262
devtricker <ayifacts@gmail.com>
2026-02-04 14:05:52 +0530

    Update ba.py

--- ba.py
+++ ba.py
@@ -113,18 +113,18 @@ def tokenize_and_auth_card(card_number, exp_month, exp_year, cvv):
         
         headers = {
             'accept': '*/*',
-            'authorization': 'Bearer eyJraWQiOiIyMDE4MDQyNjE2LXByb2R1Y3Rpb24iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFsZyI6IkVTMjU2In0.eyJleHAiOjE3Njk5NTY3MjIsImp0aSI6IjI0Zjg0MTE0LWEyZmUtNDczZS05YTMwLWUyODJmY2Y4OGJhNyIsInN1YiI6ImRxaDVueHZud3ZtMnFxamgiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImRxaDVueHZud3ZtMnFxamgiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZSwidmVyaWZ5X3dhbGxldF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0IiwiQnJhaW50cmVlOkNsaWVudFNESyJdLCJvcHRpb25zIjp7Im1lcmNoYW50X2FjY291bnRfaWQiOiJiZXN0b3BwcmVtaXVtYWNjZXNzb3JpZXNncm91cF9pbnN0YW50IiwicGF5cGFsX2NsaWVudF9pZCI6IkFhbmJtNXpHVC1DTWtSNUFKS0o5UjBMa3RQcWxYSW96RENDNTNMQ2EyM3NBVXd0akRBandHM3BsVG1HNy1EanRSM2NGdXZwNEpKLUZ3VjVlIn19.bzSgStA3oWG5XYR2fGrV1BV7BH4LZM12w3L-MCdPjlLOpmZRhVpfX5fp9Xug561u4OYkeABmRnDwRIwy-0lKug',
+            'authorization': 'Bearer eyJraWQiOiIyMDE4MDQyNjE2LXByb2R1Y3Rpb24iLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsImFsZyI6IkVTMjU2In0.eyJleHAiOjE3NzAyODAwODEsImp0aSI6IjY4YzUwZTZlLWY2N2EtNDA5ZC1hYTI0LTE3ZGVmMmQxNzU4ZCIsInN1YiI6ImRxaDVueHZud3ZtMnFxamgiLCJpc3MiOiJodHRwczovL2FwaS5icmFpbnRyZWVnYXRld2F5LmNvbSIsIm1lcmNoYW50Ijp7InB1YmxpY19pZCI6ImRxaDVueHZud3ZtMnFxamgiLCJ2ZXJpZnlfY2FyZF9ieV9kZWZhdWx0IjpmYWxzZSwidmVyaWZ5X3dhbGxldF9ieV9kZWZhdWx0IjpmYWxzZX0sInJpZ2h0cyI6WyJtYW5hZ2VfdmF1bHQiXSwic2NvcGUiOlsiQnJhaW50cmVlOlZhdWx0IiwiQnJhaW50cmVlOkNsaWVudFNESyJdLCJvcHRpb25zIjp7Im1lcmNoYW50X2FjY291bnRfaWQiOiJiZXN0b3BwcmVtaXVtYWNjZXNzb3JpZXNncm91cF9pbnN0YW50IiwicGF5cGFsX2NsaWVudF9pZCI6IkFhbmJtNXpHVC1DTWtSNUFKS0o5UjBMa3RQcWxYSW96RENDNTNMQ2EyM3NBVXd0akRBandHM3BsVG1HNy1EanRSM2NGdXZwNEpKLUZ3VjVlIn19.wSQT0EeEwJmLJvi1nEktETh1RqN0SDsaKldqWOnbi8L8lVs4_zj_9KnbhdIYRY3kAmluW5diZQA2nO3-TXnWRw',
             'braintree-version': '2018-05-10',
             'content-type': 'application/json',
-            'origin': 'https://www.calipercovers.com',
-            'referer': 'https://www.calipercovers.com/',
+            'origin': 'https://assets.braintreegateway.com',
+            'referer': 'https://assets.braintreegateway.com/',
         }
         
         json_data = {
             'clientSdkMetadata': {
                 'source': 'client',
                 'integration': 'custom',
-                'sessionId': f'session_{int(time.time())}',
+                'sessionId': f'f5ba0a46-b418-41ff-994b-d4ac16c7{int(time.time() * 1000)}',
             },
             'query': 'mutation TokenizeCreditCard($input: TokenizeCreditCardInput!) { tokenizeCreditCard(input: $input) { token creditCard { bin brandCode last4 } } }',
             'variables': {
@@ -199,12 +199,41 @@ def add_payment_method(payment_nonce):
         log("🌐 Connecting to Calipercovers...", "info")
         
         cookies = {
-            'cf_clearance': 'BHxMJGw4c1CtFmIoOmVo.3cQtsR4cekQsTSxi1KdsbM-1769870325-1.2.1.1-.Yz0tKTKushrpZ7axJmuV4A2u7Kly8KA0fHdrzlWAjo8beGmbyNS10_UFAlqcxBaf0j9tQjZnIuY2UCrYNebNsGP3gPx9fiKR7ZTdLyoLFWChwy2FLOoqcNiQdKv_fLNciLzuw0nrJ6sgJPaXmOBggkPseTHQ0TILQ.vfnXI9H9r3ZNWBPETARuBS_xxLSuc0yBAU5dYX6JL_.AXJ3BWv1w0uciwU.xkgPRY.3dANd8',
+            'checkout_continuity_service': '0a3d1867-f110-49bd-bfcd-ef8badf28651',
+            '_fbp': 'fb.1.1768568262457.54190328299053764',
+            '_ga': 'GA1.1.1016044308.1768568263',
+            'ccid.90027420': '351176264.7772418580',
+            '__attentive_id': 'eee71347810f4c19a43dc170f71fe29d',
+            '_attn_': 'eyJ1Ijoie1wiY29cIjoxNzY4NTY4MjY0NzgxLFwidW9cIjoxNzY4NTY4MjY0NzgxLFwibWFcIjoyMTkwMCxcImluXCI6ZmFsc2UsXCJ2YWxcIjpcImVlZTcxMzQ3ODEwZjRjMTlhNDNkYzE3MGY3MWZlMjlkXCJ9In0=',
+            '__attentive_cco': '1768568264785',
+            'attntv_mstore_email': 'syjjdsri@denipl.com:0',
+            '_gcl_au': '1.1.113892610.1768568262.2135844664.1769870101.1769870114',
             'wordpress_logged_in_9a06d022e5a0d800df86e500459c6102': 'dhasjdajkdnj%7C1771079717%7C5ol9n1sJvi9pCr2TZGURwDsTkYEtelYxTyvfMdgat8y%7C49b16285c781c94b56f748cafcc323ce32760af118e387ae999e157efd6878e9',
+            '__kla_id': 'eyJjaWQiOiJNVFV6WmpCa1pEa3RZV0poWlMwMFpETmpMVGt5T0RFdFlqRmhaRFEwTVRRd05qY3kiLCIkZXhjaGFuZ2VfaWQiOiJqdmRFRUZlZ0t2a3RReWpUbjA0VlpvNmdXSEZUTVNzUlMwN2dKZ3lFaWpzLkt4ZFJHViJ9',
+            'wcacr_user_country': 'IN',
+            'wfwaf-authcookie-0cfc0dfc6182cc86058203ff9ed084fe': '1186786%7Cother%7Cread%7C4635efaa2b31906322f22633e26df0f5294c2f5fe37e3448d35f907a25278252',
+            'sbjs_migrations': '1418474375998%3D1',
+            'sbjs_current_add': 'fd%3D2026-02-04%2007%3A57%3A35%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.calipercovers.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+            'sbjs_first_add': 'fd%3D2026-02-04%2007%3A57%3A35%7C%7C%7Cep%3Dhttps%3A%2F%2Fwww.calipercovers.com%2Fmy-account%2F%7C%7C%7Crf%3D%28none%29',
+            'sbjs_current': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+            'sbjs_first': 'typ%3Dtypein%7C%7C%7Csrc%3D%28direct%29%7C%7C%7Cmdm%3D%28none%29%7C%7C%7Ccmp%3D%28none%29%7C%7C%7Ccnt%3D%28none%29%7C%7C%7Ctrm%3D%28none%29%7C%7C%7Cid%3D%28none%29%7C%7C%7Cplt%3D%28none%29%7C%7C%7Cfmt%3D%28none%29%7C%7C%7Ctct%3D%28none%29',
+            'cf_clearance': 'Z_9VidN.uL5O6Esp4r26XYQJEqT7CcLd.xUvNgwwzjY-1770193683-1.2.1.1-EGwdvHnSWM7oyiQNR8ynkH0aWdWAtRJcATMa3Wx_vk50Pu8HPOItlVRpkuOSahdUxepur6X25sVzzN1995cejwDWzsnPuZCOBvIp3mZyr7PAWZgrZYoIdxMC8WZqiDWVTAAFPtE2njH22mWAyvkNFw13Jp.0Sa6Oyay2ShNdgmJIvYQ2YqzonSUXD0c6kATiE7lrB3NgUF14sp8_t9.6pdOeZLYV6P6u6.LEP83nbcM',
+            'yotpo_pixel': 'a26547c2-2c52-4c29-a300-0b90640b33ac',
+            '_sp_ses.d8f1': '*',
+            '__attentive_session_id': '756f1f8997cc48058ffdd658a9110c8c',
+            '__attentive_ss_referrer': 'ORGANIC',
+            '__attentive_dv': '1',
+            'rl_visitor_history': 'a744769e-12da-4105-9e83-8adac74ea5c7',
+            'sbjs_session': 'pgs%3D5%7C%7C%7Ccpg%3Dhttps%3A%2F%2Fwww.calipercovers.com%2Fmy-account%2Fadd-payment-method%2F',
+            '_sp_id.d8f1': '4e6928ae26c29cd3.1768568262.7.1770193683.1769870404',
+            '__attentive_pv': '5',
+            '_uetsid': '5c9633e001a311f1a78bc36a8a626d88',
+            '_uetvid': 'f28d64c0f2da11f08352e5873f8901fb',
+            '_ga_9VQF57TW94': 'GS2.1.s1770193656$o7$g1$t1770193790$j60$l0$h0',
         }
         
         headers = {
-            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
             'content-type': 'application/x-www-form-urlencoded',
             'origin': 'https://www.calipercovers.com',
             'referer': 'https://www.calipercovers.com/my-account/add-payment-method/',
@@ -214,10 +243,10 @@ def add_payment_method(payment_nonce):
         data = {
             'payment_method': 'braintree_cc',
             'braintree_cc_nonce_key': payment_nonce,
-            'braintree_cc_device_data': f'{{"device_session_id":"{int(time.time())}","fraud_merchant_id":null}}',
+            'braintree_cc_device_data': f'{{"device_session_id":"3e42bdc91fa6e23debf1ff21564e53c3","fraud_merchant_id":null,"correlation_id":"f5ba0a46-b418-41ff-994b-d4ac16c7"}}',
             'braintree_cc_3ds_nonce_key': '',
             'braintree_cc_config_data': '{"environment":"production","merchantId":"dqh5nxvnwvm2qqjh"}',
-            'woocommerce-add-payment-method-nonce': '68c7586bf3',
+            'woocommerce-add-payment-method-nonce': '655e0182f4',
             '_wp_http_referer': '/my-account/add-payment-method/',
             'woocommerce_add_payment_method': '1',
         }
